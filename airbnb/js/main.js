54ea6d9166e9070878ab4799ca3f2bb616861a49
Amrahdamirp <adhie.scythe@gmail.com>
2016-11-26 15:11:57 +0700

    Add files via upload

--- airbnb/js/main.js
+++ airbnb/js/main.js
@@ -1,100 +1,100 @@
-(function() {
-	var support = { animations : Modernizr.cssanimations },
-		animEndEventNames = {
-			'WebkitAnimation' : 'webkitAnimationEnd',
-			'OAnimation' : 'oAnimationEnd',
-			'msAnimation' : 'MSAnimationEnd',
-			'animation' : 'animationend'
-		},
-		// animation end event name
-		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
-		effectSel = document.getElementById( 'fxselect' ),
-		component = document.getElementById( 'component' ),
-		items = component.querySelector( 'ul.itemwrap' ).children,
-		current = 0,
-		itemsCount = items.length,
-		nav = component.querySelector( 'nav' ),
-		navNext = nav.querySelector( '.next' ),
-		navPrev = nav.querySelector( '.prev' ),
-		isAnimating = false;
-
-	function init() {
-		showNav();
-		changeEffect();
-		navNext.addEventListener( 'click', function( ev ) { ev.preventDefault(); navigate( 'next' ); } );
-		navPrev.addEventListener( 'click', function( ev ) { ev.preventDefault(); navigate( 'prev' ); } );
-		effectSel.addEventListener( 'change', changeEffect );
-	}
-
-	function hideNav() {
-		nav.style.display = 'none';
-	}
-
-	function showNav() {
-		nav.style.display = 'block';
-	}
-
-	function changeEffect() {
-		component.className = component.className.replace(/\bfx.*?\b/g, '');
-		classie.addClass( component,'fxPressAway');
-	}
-
-	$(function(){
-		$('#menu').slicknav();
-	});
-
-	function navigate( dir ) {
-		//if( isAnimating || !effectSel.selectedIndex ) return false;
-		isAnimating = true;
-		var cntAnims = 0;
-
-
-		var currentItem = items[ current ];
-
-		if( dir === 'next' ) {
-			current = current < itemsCount - 1 ? current + 1 : 0;
-		}
-		else if( dir === 'prev' ) {
-			current = current > 0 ? current - 1 : itemsCount - 1;
-		}
-
-		var nextItem = items[ current ];
-
-		var onEndAnimationCurrentItem = function() {
-			this.removeEventListener( animEndEventName, onEndAnimationCurrentItem );
-			classie.removeClass( this, 'current' );
-			classie.removeClass( this, dir === 'next' ? 'navOutNext' : 'navOutPrev' );
-			++cntAnims;
-			if( cntAnims === 2 ) {
-				isAnimating = false;
-			}
-		}
-
-		var onEndAnimationNextItem = function() {
-			this.removeEventListener( animEndEventName, onEndAnimationNextItem );
-			classie.addClass( this, 'current' );
-			classie.removeClass( this, dir === 'next' ? 'navInNext' : 'navInPrev' );
-			++cntAnims;
-			if( cntAnims === 2 ) {
-				isAnimating = false;
-			}
-		}
-
-		if( support.animations ) {
-			currentItem.addEventListener( animEndEventName, onEndAnimationCurrentItem );
-			nextItem.addEventListener( animEndEventName, onEndAnimationNextItem );
-		}
-		else {
-			onEndAnimationCurrentItem();
-			onEndAnimationNextItem();
-		}
-
-		classie.addClass( currentItem, dir === 'next' ? 'navOutNext' : 'navOutPrev' );
-		classie.addClass( nextItem, dir === 'next' ? 'navInNext' : 'navInPrev' );
-	}
-
-	init();
-})();
+//(function() {
+//	var support = { animations : Modernizr.cssanimations },
+//		animEndEventNames = {
+//			'WebkitAnimation' : 'webkitAnimationEnd',
+//			'OAnimation' : 'oAnimationEnd',
+//			'msAnimation' : 'MSAnimationEnd',
+//			'animation' : 'animationend'
+//		},
+//		// animation end event name
+//		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+//		effectSel = document.getElementById( 'fxselect' ),
+//		component = document.getElementById( 'component' ),
+//		items = component.querySelector( 'ul.itemwrap' ).children,
+//		current = 0,
+//		itemsCount = items.length,
+//		nav = component.querySelector( 'nav' ),
+//		navNext = nav.querySelector( '.next' ),
+//		navPrev = nav.querySelector( '.prev' ),
+//		isAnimating = false;
+//
+//	function init() {
+//		showNav();
+//		changeEffect();
+//		navNext.addEventListener( 'click', function( ev ) { ev.preventDefault(); navigate( 'next' ); } );
+//		navPrev.addEventListener( 'click', function( ev ) { ev.preventDefault(); navigate( 'prev' ); } );
+//		effectSel.addEventListener( 'change', changeEffect );
+//	}
+//
+//	function hideNav() {
+//		nav.style.display = 'none';
+//	}
+//
+//	function showNav() {
+//		nav.style.display = 'block';
+//	}
+//
+//	function changeEffect() {
+//		component.className = component.className.replace(/\bfx.*?\b/g, '');
+//		classie.addClass( component,'fxPressAway');
+//	}
+//
+//	$(function(){
+//		$('#menu').slicknav();
+//	});
+//
+//	function navigate( dir ) {
+//		//if( isAnimating || !effectSel.selectedIndex ) return false;
+//		isAnimating = true;
+//		var cntAnims = 0;
+//
+//
+//		var currentItem = items[ current ];
+//
+//		if( dir === 'next' ) {
+//			current = current < itemsCount - 1 ? current + 1 : 0;
+//		}
+//		else if( dir === 'prev' ) {
+//			current = current > 0 ? current - 1 : itemsCount - 1;
+//		}
+//
+//		var nextItem = items[ current ];
+//
+//		var onEndAnimationCurrentItem = function() {
+//			this.removeEventListener( animEndEventName, onEndAnimationCurrentItem );
+//			classie.removeClass( this, 'current' );
+//			classie.removeClass( this, dir === 'next' ? 'navOutNext' : 'navOutPrev' );
+//			++cntAnims;
+//			if( cntAnims === 2 ) {
+//				isAnimating = false;
+//			}
+//		}
+//
+//		var onEndAnimationNextItem = function() {
+//			this.removeEventListener( animEndEventName, onEndAnimationNextItem );
+//			classie.addClass( this, 'current' );
+//			classie.removeClass( this, dir === 'next' ? 'navInNext' : 'navInPrev' );
+//			++cntAnims;
+//			if( cntAnims === 2 ) {
+//				isAnimating = false;
+//			}
+//		}
+//
+//		if( support.animations ) {
+//			currentItem.addEventListener( animEndEventName, onEndAnimationCurrentItem );
+//			nextItem.addEventListener( animEndEventName, onEndAnimationNextItem );
+//		}
+//		else {
+//			onEndAnimationCurrentItem();
+//			onEndAnimationNextItem();
+//		}
+//
+//		classie.addClass( currentItem, dir === 'next' ? 'navOutNext' : 'navOutPrev' );
+//		classie.addClass( nextItem, dir === 'next' ? 'navInNext' : 'navInPrev' );
+//	}
+//
+//	init();
+//})();
 
 // Starrr plugin (https://github.com/dobtco/starrr)
 var __slice = [].slice;
@@ -210,4 +210,69 @@ $( document ).ready(function() {
 	$('#hearts-existing').on('starrr:change', function(e, value){
 		$('#count-existing').html(value);
 	});
+});
+
+//------ main nav menu ----- //
+$(document).ready(function() {
+	$(window).scroll(function () {
+
+		function checkWidth() {
+			var windowSize = $(window).width();
+
+			if (windowSize > 768) {
+				if (jQuery(this).scrollTop() > 400) {
+
+					$('.navbar').addClass('navbar-scrolled-down');
+					//  $('.logo').css({'z-index':'99', 'position':'fixed', 'top':'11px'});
+				} else {
+					$('.navbar').removeClass('navbar-scrolled-down');
+					//  $('.logo').css({'z-index':'99', 'position':'absolute', 'top':'-45px'});
+				}
+			}
+
+		}
+
+		// Execute on load
+		checkWidth();
+		// Bind event listener
+		$(window).resize(checkWidth);
+
+	});
+
+
+	$(".next-slider").click(function(){
+		var val = $(this).attr('value');
+		var countslider = $('#slider'+val).attr('value');
+		countslider = parseFloat(countslider)+ parseFloat(-25);
+		$("#slider"+val).css({'transform': 'translateX('+countslider+'%)'});
+		$("#prev-slider"+val).show();
+		$('#slider'+val).attr('value',countslider);
+		if(countslider == -50){
+			$("#next-slider"+val).hide();
+		}else{
+			$("#next-slider"+val).show();
+		}
+	});
+
+	$(".prev-slider").click(function(){
+		var val = $(this).attr('value');
+		var countslider = $('#slider'+val).attr('value');
+		countslider = parseFloat(countslider)+ parseFloat(25);
+		$("#slider"+val).css({'transform': 'translateX('+countslider+'%)'});
+		$("#prev-slider"+val).show();
+		$('#slider'+val).attr('value',countslider);
+		if(countslider == 0){
+			$("#prev-slider"+val).hide();
+			$("#next-slider"+val).show();
+		}else{
+			$("#prev-slider"+val).show();
+			$("#next-slider"+val).show();
+		}
+	});
+	if(countslider == 0){
+		$(".prev-slider").hide();
+	}
+
+
+
 });
